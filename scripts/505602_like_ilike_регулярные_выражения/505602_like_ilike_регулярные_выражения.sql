b4d890679aa9d35c13f1766f94784c8856ce7250
aleksei-korotkov <aleksei-korotkov@yandex.ru>
2026-08-26 23:08:07 +0300

    Update 505602_like_ilike_регулярные_выражения.sql

--- scripts/505602_like_ilike_регулярные_выражения/505602_like_ilike_регулярные_выражения.sql
+++ scripts/505602_like_ilike_регулярные_выражения/505602_like_ilike_регулярные_выражения.sql
@@ -1,1 +1,38 @@
+CREATE TABLE vacancies
+(
+    id          BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title       TEXT        NOT NULL,
+    company     TEXT        NOT NULL,
+    description TEXT,
+    created_at  TIMESTAMPTZ NOT NULL DEFAULT now()
+);
 
+SELECT id, name, email
+FROM users
+WHERE email LIKE '%mail%';
+
+SELECT id, name, price
+FROM products
+WHERE name ILIKE '%air%';
+
+SELECT id, name, price
+FROM products
+WHERE name ILIKE 'i%';
+
+SELECT id, name, price
+FROM products
+WHERE name ILIKE '%pro';
+
+SELECT id, name, email
+FROM users
+WHERE email ILIKE 'A%'
+   OR email ILIKE 'I%';
+
+SELECT id, title, company, description
+FROM vacancies
+WHERE title ~* '(java|go|postgres)'
+   OR description ~* '(java|go|postgres)';
+
+SELECT id, name, price
+FROM products
+WHERE name ~* '^iPhone [0-9]+';
